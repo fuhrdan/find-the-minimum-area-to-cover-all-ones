9f7f1c57fef0faf19c7a5f3512711c6bfe8185e5
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-08-22 06:52:19 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//*****************************************************************************
+//** 3195. Find the Minimum Area to Cover All Ones I                leetcode **
+//*****************************************************************************
+
+int minimumArea(int** grid, int gridSize, int* gridColSize) {
+    int minRow = INT_MAX, maxRow = INT_MIN;
+    int minCol = INT_MAX, maxCol = INT_MIN;
+
+    for (int i = 0; i < gridSize; i++)
+    {
+        for (int j = 0; j < gridColSize[i]; j++)
+        {
+            if (grid[i][j] == 1)
+            {
+                if (i < minRow) minRow = i;
+                if (i > maxRow) maxRow = i;
+                if (j < minCol) minCol = j;
+                if (j > maxCol) maxCol = j;
+            }
+        }
+    }
+
+    int height = maxRow - minRow + 1;
+    int width  = maxCol - minCol + 1;
+    int retval = height * width;
+
+    return retval;
+}
